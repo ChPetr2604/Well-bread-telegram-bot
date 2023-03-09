07c0b4d83ecad51b50fbcad4be5df462eb03cad2
Chemodanov Petr <102319069+ChPetr2604@users.noreply.github.com>
2023-03-09 19:58:52 +0300

    Update Electronic_Diary_Telegram_Bot_Project.py

--- Electronic_Diary_Telegram_Bot_Project.py
+++ Electronic_Diary_Telegram_Bot_Project.py
@@ -51,7 +51,7 @@ alt_timetable = {
 rus_list = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я']
 con = psycopg2.connect(config['database_url'], sslmode="require")
 cur = con.cursor()
-a = 
+a = []
 
 
 class ClassFormatError(Exception):
