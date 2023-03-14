bdc020121fbca70892a42d82202bacc73a4b8a60
Chemodanov Petr <102319069+ChPetr2604@users.noreply.github.com>
2023-03-14 21:30:55 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 config = {
-    'token': '6084821255:AAFu5sxwdFBXQzNyER6b8A_pTzJ1lFBMWjU',
-    'database_url': "postgresql://postgres:fYij0YoaZC7BhI8MyjH7@containers-us-west-150.railway.app:7135/railway"
+    'token': '6084821255:AAH7TdK4_Qb1mQuHNSp98a2thbhRJ-eXVSM',
+    'database_url': "postgresql://postgres:J1p7qDxnRtuvDYIW3EQq@containers-us-west-198.railway.app:7375/railway"
 }
